0b8c8e2e87d57bae1c92df7e03f1128778631062
MisterAndry <mister.andry@mail.ru>
2016-05-23 18:15:37 +0300

    refactoring

--- Cities.py
+++ Cities.py
@@ -1,86 +1,91 @@
 # -*- coding: utf-8 -*-
+class Cities(object):
+    db_cities = {}                                           # словарь с городами
+    dnt_use_let = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')  # буквы, с которых не начинаются города в словаре
+    current_city = ''                                        # текущий названный город
 
+    def __init__(self, file_name):
+        # считываем города и помещаем их в словарь
+        # изначально в словаре каждому городу поставлено значение 1,
+        # когда город называется, значение становится 0
+        with open(file_name, 'r', encoding="utf-8") as f:
+            data_tmp = f.read().splitlines()
+            if len(data_tmp) < 10:
+                print('В файле менее десяти городов. Дополните его.')
+                exit()
+            self.db_cities = {a: 1 for a in data_tmp}
 
-# какая справа по счёту буква текущего города будет использоваться для следующего города
-def which_letter(city, dnt_use_let):
-    count = -1
-    while dnt_use_let.count(city[count]) != 0:
-        count -= 1
-    return count
+            # буквы, с которых НЕ начинаются города во входном файле
+            for key in self.db_cities.keys():
+                letter = key[0]
+                if self.dnt_use_let.count(letter) != 0:
+                    self.dnt_use_let.remove(letter)
+            # первый ход за программой
+            print('Игра Города!')
+            self.current_city = next(iter(self.db_cities.keys()))
+            self.db_cities[self.current_city] = 0
+            print(self.current_city)
 
+    # на какую букву надо назвать город
+    @staticmethod
+    def which_letter(city, dnt_use_let):
+        count = -1
+        while dnt_use_let.count(city[count]) != 0:
+            count -= 1
+        return city[count]
 
-# окончена ли игра (игра окначивается если больше нет городов, начинающихся на нужную букву)
-def is_game_over(db_cities, city, dnt_use_let):
-    cur_let = city[which_letter(city, dnt_use_let)]
-    for ct, value in db_cities.items():
-        if value == 1 and ct[0] == cur_let:
-            return False
-    return True
+    # окончена ли игра (игра окначивается если больше нет городов, начинающихся на нужную букву)
+    def is_game_over(self):
+        cur_let = self.which_letter(self.current_city, self.dnt_use_let)
+        for city, value in self.db_cities.items():
+            if value == 1 and city[0] == cur_let:
+                return False
+        return True
 
+    # ход программы
+    def pc_turn(self):
+        cur_let = self.which_letter(self.current_city, self.dnt_use_let)
+        for city, value in self.db_cities.items():
+            if value == 1 and city[0] == cur_let:
+                self.db_cities[city] = 0
+                self.current_city = city
+                print(city)
+                return
 
-# ход программы
-def pc_turn(db_cities, city, dnt_use_let):
-    cur_let = city[which_letter(city, dnt_use_let)]
-    for ct, value in db_cities.items():
-        if value == 1 and ct[0] == cur_let:
-            db_cities[ct] = 0
-            print(ct)
-            return ct
-
-
-# ход игрока
-def user_turn(db_cities, city, dnt_use_let):
-    cnt = which_letter(city, dnt_use_let)
-    print('Веедите город, начинающийся на \'' + city[cnt] + '\':')
-    # проверка на корректность введённого города
-    wrong = True
-    while wrong:
-        wrong = False
-        user_city = input()
-        user_city = user_city.lower().strip()
-        if db_cities.get(user_city) is None:
-            print('Такой город не существует. Введите другой.')
-            wrong = True
-            continue
-        for ct, value in db_cities.items():
-            if user_city == ct and value == 0:
-                print('Данный город уже назывался. Введите другой.')
+    # ход игрока
+    def user_turn(self):
+        cur_let = self.which_letter(self.current_city, self.dnt_use_let)
+        print('Веедите город, начинающийся на \'' + cur_let + '\':')
+        # проверка на корректность введённого города
+        wrong = True
+        while wrong:
+            wrong = False
+            user_city = input().lower().strip()
+            if user_city == '' or user_city[0] != cur_let:
+                print('Ваш город должен начинаьться на \'' + cur_let + '\'!')
                 wrong = True
-                break
-    data[user_city] = 0
-    return user_city
+                continue
+            if self.db_cities.get(user_city) is None:
+                print('Такой город не существует. Введите другой.')
+                wrong = True
+                continue
+            for ct, value in self.db_cities.items():
+                if user_city == ct and value == 0:
+                    print('Данный город уже назывался. Введите другой.')
+                    wrong = True
+                    break
+        self.db_cities[user_city] = 0
+        self.current_city = user_city
 
 
-# считываем города и помещаем их в словарь
-# изначально в словаре каждому городу поставлено значение 1, когда город называется, значение становится 0
-with open('ru_cities', 'r', encoding="utf-8") as f:
-    data_tmp = f.read().splitlines()
-    if len(data_tmp) < 2:
-        print('В файле менее двух городов!')
-        exit()
-    data = {a: 1 for a in data_tmp}
-
-    # находим буквы, с которых НЕ начинаются города во входном файле
-    no_use_letters = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
-    for key in data.keys():
-        letter = key[0]
-        if no_use_letters.count(letter) != 0:
-            no_use_letters.remove(letter)
-
-    # программа ходит первой
-    print('Игра Города!')
-    next_city = next(iter(data.keys()))
-    data[next_city] = 0
-    print(next_city)
-    # основной цикл
-    while True:
-        next_city = user_turn(data, next_city, no_use_letters)
-        if is_game_over(data, next_city, no_use_letters):
-            print('You win!')
-            break
-
-        next_city = pc_turn(data, next_city, no_use_letters)
-        if is_game_over(data, next_city, no_use_letters):
-            print('You lose!')
-            break
-    exit()
+game = Cities('ru_cities')
+while True:
+    game.user_turn()
+    if game.is_game_over():
+        print('You win!')
+        break
+    game.pc_turn()
+    if game.is_game_over():
+        print('You lose!')
+        break
+exit()
